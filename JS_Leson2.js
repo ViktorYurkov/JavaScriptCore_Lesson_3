7b13c9ceae0969836ff839cc07ac6484376f4909
Viktor <HelpsDIA@gmail.com>
2020-01-16 22:19:34 +0200

    added code example from lesson 2

---
+++ JS_Leson2.js
@@ -0,0 +1,61 @@
+function show(data) {
+    console.log(data);
+}
+
+//Завдання1
+var person = prompt("Введіть значення змінної");
+if (person > 0) {
+    show(1);
+} else if (person < 0) {
+    show(-1);
+} else {
+    show(0);
+}
+
+//Завдання2
+var login = prompt("Who's there?") ; 
+if (login == "Admin" ) {
+    var password = prompt("Password?");
+    if (password == "TheMaster" ) {
+        show('Welcome!');
+    } else if (password == "Other") {
+        show('Wrong password');
+    } else if (password == "Cancel") {
+        show('Canceled');
+    } 
+} else if (login == "Other" ) {
+    show("I don't know you");
+} else if (login == "Cancel" ) {
+    show("Canceled");
+}
+
+//Завдання3
+var login = prompt("Who's there?") ;
+switch(login) {   
+    case "Admin" : {
+    var password = prompt("Password?");
+    switch(password) {    
+    case "TheMaster" : {
+        show('Welcome!');
+        break;
+    }
+    case "Other" : {
+        show('Wrong password');
+        break;
+    }
+    case "Cancel" : {
+        show('Canceled');
+        break;
+    }
+    }
+        break;
+}
+    case "Other" : {
+        show("I don't know you");
+        break;
+}
+    case "Cancel" : {
+        show("Canceled");
+        break;
+}
+}
